90340487b93bfa23c3032b13f7357ddb0d93a3b5
aashmauprety <aashmauprety99@gmail.com>
2021-11-21 18:00:21 -0500

    Added Callback

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tensorflow.keras.optimizers import Adam
 from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
+from rl.callbacks import ModelIntervalCheckpoint, FileLogger
 
 weight = read_data()
 env = findmeEnv(weight)
@@ -14,14 +15,26 @@ states,actions = get_shape()
 model = build_model(states,actions)
 print(model.summary)
 
+# Define call backs
+ENV_NAME = 'RL_IoT-v1'
+def build_callbacks(ENV_NAME):
+    checkpoint_weights_filename = 'dqn_' + ENV_NAME + '_weights_{step}.h5f'
+    log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+    return callbacks
+
+
 # Define RL agent 
-def build_agent(model, actions):
-  policy = BoltzmannQPolicy()
-  memory = SequentialMemory(limit=50000, window_length = 1)
-  dqn = DQNAgent(model = model, memory = memory, policy=policy, nb_actions=actions, nb_steps_warmup = 10, target_model_update = 1e-2)
-  return dqn
+policy = BoltzmannQPolicy()
+memory = SequentialMemory(limit=50000, window_length = 1)
+dqn = DQNAgent(model=model, nb_actions=actions, policy=policy, memory=memory, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+                   train_interval=4, delta_clip=1.)
+dqn.compile(Adam(lr=.00025), metrics=['mae'])
+callbacks = build_callbacks(ENV_NAME)
+
+#Start Training
+dqn.fit(env, nb_steps=20000, visualize=False,verbose=2, callbacks=callbacks)
+dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 
-dqn = build_agent(model, actions)
-dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
-dqn.fit(env, nb_steps=50, visualize=False,verbose=1)
